ab130b3748b0ccca09c3e78901ad8301a59a0844
EC2 Default User <ec2-user@ip-172-31-36-23.us-east-2.compute.internal>
2019-06-06 19:59:26 +0000

    go

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://paypalvanilla.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => 'base64:uV6HYUfqW7pQ2WVW8wzitsursMR8uFk1I1PO+q0kcO0=',
 
     'cipher' => 'AES-256-CBC',
 
